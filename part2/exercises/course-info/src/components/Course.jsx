492a5a31cc302b4b515f7bbd4dadb9e4214066ae
Serafim Kvashnin <serafim274715@gmail.com>
2024-09-01 20:12:33 +0300

    Complete exercises 2.3-2.5

--- part2/exercises/course-info/src/components/Course.jsx
+++ part2/exercises/course-info/src/components/Course.jsx
@@ -1,7 +1,3 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
-
 const Course = ({ course }) => {
     return <>
         <Header name={course.name} />
@@ -9,5 +5,23 @@ const Course = ({ course }) => {
         <Total parts={course.parts} />
     </>
 }
+
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Content = ({ parts }) => parts.map((part) => 
+    <Part 
+        key={part.id} 
+        part={part}
+    />
+)
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Total = ({ parts }) => {
+    const exercises = parts.reduce((sum, part) => 
+        sum + part.exercises, 0)
+    return <strong>Total of {exercises} exercises</strong>
+}
+
 
 export default Course
--- part2/exercises/course-info/src/components/Total.jsx
+++
@@ -1,7 +0,0 @@
-const Total = ({ parts }) => {
-    const exercises = parts.reduce((sum, part) => 
-        sum + part.exercises, 0)
-    return <strong>Total of {exercises} exercises</strong>
-}
-
-export default Total
