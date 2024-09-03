48626e0f9a7c4af136de5ec96e5a7ebadf642557
Serafim Kvashnin <serafim274715@gmail.com>
2024-09-03 16:20:15 +0300

    Complete exercises 2.8-2.10

--- part2/exercises/the-phonebook/src/App.jsx
+++ part2/exercises/the-phonebook/src/App.jsx
@@ -2,44 +2,86 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas' 
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
-  const [newName, setNewName] = useState('')
 
-  const addPerson = (event) => {
+  const [filter, setFilter] = useState('')
+
+  const handleAddPerson = (event) => {
     event.preventDefault()
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    const form = new FormData(event.target)
+    const name = form.get('name')
+    const number = form.get('number')
+    if (persons.some((person) => person.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already added to phonebook`)
       return
     }
     const newPerson = {
-      name: newName
+      id: persons.length + 1,
+      name: name,
+      number: number
     }
     setPersons(persons.concat(newPerson))
-    setNewName('')
+  }
+
+  const getFilteredPersons = (filter) => persons.filter((person) => 
+    `${person.name} ${person.number}`.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const handleFilter = (event) => {
+    const newFilter = event.target.value    
+    setFilter(newFilter)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: 
-          <input 
-            value={newName}
-            onChange={(event) => setNewName(event.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      <h1>Phonebook</h1>
+      <PersonForm onAddPerson={handleAddPerson} />
+      <Filter onFilter={handleFilter} />
+      <Persons persons={getFilteredPersons(filter)} />
     </div>
   )
 }
+
+const Filter = ({ onFilter }) => <div>
+  <h2>Filter</h2>
+  <label>Filter shown with
+    <input name="filter" onChange={onFilter}/>
+  </label>
+</div>
+
+
+const PersonForm = ({ onAddPerson }) => <div>
+  <h2>Add</h2>
+  <form onSubmit={onAddPerson}>
+    <div>
+      name: 
+      <input name="name" />
+    </div>
+    <div>
+      number: 
+      <input name="number" />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+</div>
+
+const Persons = ({ persons }) => <div>
+  <h2>Numbers</h2>
+  <div>
+    {persons.map((person) => 
+      <PersonDetails key={person.id} person={person} />
+    )}
+  </div>
+</div>
+
+const PersonDetails = ({ person }) => <p>
+  {person.name} {person.number}
+</p>
 
 export default App
