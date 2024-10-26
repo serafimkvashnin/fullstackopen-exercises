f8f3466fc02792b88fcdcc48b94783241a1d2d43
Serafim Kvashnin <serafim274715@gmail.com>
2024-10-27 01:09:11 +0300

    Notes app frontend login

--- part2/course/a/src/App.jsx
+++ part2/course/a/src/App.jsx
@@ -3,109 +3,167 @@ import Note from './components/Note';
 import Notification from './components/Notification';
 import { useEffect } from 'react';
 import noteService from './services/notes';
+import loginService from './services/login';
 
 const App = () => {
-    const [notes, setNotes] = useState([]);
-    const [newNoteText, setNewNoteText] = useState('a new note...');
-    const [showAll, setShowAll] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
+  const [notes, setNotes] = useState([]);
+  const [newNoteText, setNewNoteText] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        noteService
-            .getAll()
-            .then((returnedNotes) => setNotes([...returnedNotes]));
-    }, []);
+  useEffect(() => {
+    noteService.getAll().then((returnedNotes) => setNotes([...returnedNotes]));
+  }, []);
 
-    const addNote = (event) => {
-        event.preventDefault();
-        const newNote = {
-            content: newNoteText,
-            important: Math.random() > 0.5,
-        };
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser');
+    if (loggedUserJSON) {
+      const loggedUser = JSON.parse(loggedUserJSON);
+      setUser(loggedUser);
+      noteService.setToken(loggedUser.token);
+    }
+  }, []);
 
-        noteService.create(newNote).then((returnedNote) => {
-            setNotes(notes.concat(returnedNote));
-            setNewNoteText('');
-        });
+  const addNote = (event) => {
+    event.preventDefault();
+    const newNote = {
+      content: newNoteText,
+      important: Math.random() > 0.5
     };
 
-    const handleToggleImportance = (note) => {
-        const newNote = {
-            ...note,
-            important: !note.important,
-        };
-        noteService
-            .put(note.id, newNote)
-            .then((returnedNote) =>
-                setNotes(
-                    notes.map((n) => (n.id !== note.id ? n : returnedNote))
-                )
-            )
-            .catch((error) => {
-                setErrorMessage(
-                    `The note '${note.content}' was already deleted from server!`
-                );
-                setNotes(notes.filter((n) => n.id !== note.id));
-            });
+    noteService.create(newNote).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNoteText('');
+    });
+  };
+
+  const handleToggleImportance = (note) => {
+    const newNote = {
+      ...note,
+      important: !note.important
     };
+    noteService
+      .put(note.id, newNote)
+      .then((returnedNote) =>
+        setNotes(notes.map((n) => (n.id !== note.id ? n : returnedNote)))
+      )
+      .catch((error) => {
+        setErrorMessage(
+          `The note '${note.content}' was already deleted from server!`
+        );
+        setNotes(notes.filter((n) => n.id !== note.id));
+      });
+  };
 
-    const notesToShow = showAll
-        ? notes
-        : notes.filter((note) => note.important);
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      window.localStorage.setItem('loggedUser', JSON.stringify(user));
+      noteService.setToken(user.token);
+    } catch (error) {
+      setErrorMessage('Invalid username or password');
+    }
+    setUsername('');
+    setPassword('');
+  };
 
-    return (
-        <>
-            <h1>Notes</h1>
-            <Notification
-                message={errorMessage}
-                onClose={() => setErrorMessage(null)}
-            />
-            <div>
-                <label>
-                    <input
-                        type="checkbox"
-                        onChange={(_) => setShowAll(!showAll)}
-                        checked={showAll}
-                    />
-                    Show all
-                </label>
-            </div>
-            <ul>
-                {notesToShow.map((note) => (
-                    <Note
-                        key={note.id}
-                        note={note}
-                        onToggleImportance={handleToggleImportance}
-                    />
-                ))}
-            </ul>
-            <form onSubmit={addNote}>
-                <input
-                    value={newNoteText}
-                    onChange={(event) => setNewNoteText(event.target.value)}
-                />
-                <button type="submit">Save</button>
-            </form>
-            <Footer />
-        </>
-    );
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        Username
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNoteText}
+        onChange={(event) => setNewNoteText(event.target.value)}
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  return (
+    <>
+      <h1>Notes</h1>
+
+      <Notification
+        message={errorMessage}
+        onClose={() => setErrorMessage(null)}
+      />
+
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
+
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            onChange={() => setShowAll(!showAll)}
+            checked={showAll}
+          />
+          Show all
+        </label>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            onToggleImportance={handleToggleImportance}
+          />
+        ))}
+      </ul>
+      <Footer />
+    </>
+  );
 };
 
 const Footer = () => {
-    const footerStyle = {
-        color: 'green',
-        fontStyle: 'italic',
-        fontSize: 16,
-    };
-    return (
-        <div style={footerStyle}>
-            <br />
-            <em>
-                Note app, Department of Computer Science, University of Helsinki
-                2024
-            </em>
-        </div>
-    );
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2024
+      </em>
+    </div>
+  );
 };
 
 export default App;
--- part2/course/a/src/services/notes.js
+++ part2/course/a/src/services/notes.js
@@ -2,12 +2,26 @@ import axios from 'axios';
 
 const baseUrl = '/api/notes';
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = newToken;
+};
+
 const getAll = () => getData(axios.get(baseUrl));
 
-const create = (note) => getData(axios.post(baseUrl, note));
+const create = async (note) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+  const response = await axios.post(baseUrl, note, config);
+  return response.data;
+};
 
 const put = (id, note) => getData(axios.put(`${baseUrl}/${id}`, note));
 
 const getData = (promise) => promise.then((response) => response.data);
 
-export default { getAll, create, put };
+export default { getAll, create, put, setToken };
